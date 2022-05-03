e4bad486d51bdde59db83d8f8aa8d0e0e091d881
kw <101356521+yimkawai@users.noreply.github.com>
2022-05-03 09:17:25 -0400

    Delete pages directory

--- pages/main.js
+++
@@ -1,8 +0,0 @@
-$(window).scroll(function(){
-    if($(window).scrollTop() > 0) {
-        $('.navbar').css('background', 'rgba(0, 0, 0, 0.5)');
-    } else {
-        $('.navbar').css('background', 'transparent');
-    }
-});
-
